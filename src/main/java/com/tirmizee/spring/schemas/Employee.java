0b9b5d152983316a33ebcd1494245ed8c243153b
GDLPratya <pratya.khaday@gmail.com>
2019-06-02 17:48:43 +0700

    init project

---
+++ src/main/java/com/tirmizee/spring/schemas/Employee.java
@@ -0,0 +1,21 @@
+package com.tirmizee.spring.schemas;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Employee", propOrder = {
+    "empId",
+    "firstName",
+    "lastName"
+}, namespace = "http://tirmizee.com/spring/ws/employees")
+public class Employee {
+	
+	protected Long empId;
+	protected String firstName;
+	protected String lastName;
+	
+	
+	
+}
